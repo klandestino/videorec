9a0289cbbb0587ce9df6acccedca1322754877c3
spurge <user@montin.net>
2010-03-29 17:34:40 +0200

    At last I got the logger to work

--- red5/webapps/simpleVideoRec/WEB-INF/src/se/klandestino/red5/applications/simpleVideoRec/Application.java
+++ red5/webapps/simpleVideoRec/WEB-INF/src/se/klandestino/red5/applications/simpleVideoRec/Application.java
@@ -3,29 +3,42 @@ package se.klandestino.red5.applications.simpleVideoRec;
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
+import org.red5.server.api.IScope;
+import org.red5.server.api.stream.IServerStream;
 import org.slf4j.Logger;
 
 public class Application extends ApplicationAdapter {
 
 	private static Logger log = Red5LoggerFactory.getLogger (Application.class, "simpleVideoRec");
 
-	public boolean appStart () {
-		log.debug ("Application started");
-		return true;
+	public boolean appStart (IScope app) {
+		boolean returnVal = super.appStart (app);
+		log.info ("Application started");
+		return returnVal;
 	}
 
-	public void appStop () {
-		log.debug ("Application stopped");
+	public void appStop (IScope app) {
+		super.appStop (app);
+		log.info ("Application stopped");
 	}
 
 	public boolean appConnect (IConnection conn, Object [] params) {
-		log.debug ("Connected with " + conn.getClient ().getId ());
-		return true;
+		boolean returnVal = super.appConnect (conn, params);
+		log.info ("Connected with " + conn.getClient ().getId ());
+		return returnVal;
 	}
 
 	public void appDisconnect (IConnection conn) {
-		log.debug ("Disconnected with " + conn.getClient ().getId ());
 		super.appDisconnect (conn);
+		log.info ("Disconnected with " + conn.getClient ().getId ());
+	}
+
+	public String publish () {
+		return "";
+	}
+
+	public String auth (String key) {
+		return "";
 	}
 
 }
