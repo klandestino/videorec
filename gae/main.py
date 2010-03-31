f38346887625da8ac36cc039cba3fb20db6e3f02
alfred <alfred@hoppish2.(none)>
2010-03-31 13:28:19 +0200

    funkar?

--- gae/main.py
+++ gae/main.py
@@ -15,28 +15,41 @@ class Recording(db.Model):
 	onetimecode = db.StringProperty()
 	created = db.DateTimeProperty(auto_now_add = True)
 
+class CrossDomainPolicy(webapp.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+		self.response.out.write('<?xml version="1.0"?>\r\n');
+		self.response.out.write('<cross-domain-policy><allow-access-from domain="*" /></cross-domain-policy>');
+
+	def post(self):
+		self.get()
+
 class Error(webapp.RequestHandler):
 	def get(self):
 		self.post()
 	
 	def post(self):
 		self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+		self.response.out.write('<?xml version="1.0"?>\r\n');
 		self.response.out.write('<red5missioncontrol error="Unknown URL"></red5missioncontrol>');
 
 class PostRequest(webapp.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+		self.response.out.write('<?xml version="1.0"?>\r\n');
 		self.response.out.write('<red5missioncontrol error="HTTP POST request required"></red5missioncontrol>');
 
 	def authorized(self):
 
 		if not self.request.POST.has_key('project'):
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="No project name given"></red5missioncontrol>');
 			return False
 
 		if not self.request.POST.has_key('secret'):
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="No project secret given"></red5missioncontrol>');
 			return False
 
@@ -51,11 +64,13 @@ class PostRequest(webapp.RequestHandler):
 
 		if self.request.POST['secret'] == '':
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="Empty secret is not allowed"></red5missioncontrol>');
 			return False
 
 		if project.secret != self.request.POST['secret']:
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="Unknown project name and/or project secret"></red5missioncontrol>');
 			return False
 
@@ -70,29 +85,38 @@ class Record(PostRequest):
 			recording.onetimecode = "".join(d)
 			recording.put()
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol><record url="rtmp://88.80.16.137/simpleVideoRec" stream="' + str(recording.created.strftime('%Y%m%d%H%M%S')) + '_' + str(recording.key()) + '_' + recording.onetimecode + '" /></red5missioncontrol>');
 
 class RecordConsume(PostRequest):
 	def post(self):
 		if not self.request.POST.has_key('stream'):
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="No stream HTTP POST variable given"></red5missioncontrol>');
 			return
 
-		parts = self.request.POST['stream'].split('_', 3)
-		recording = Recording.get(parts[1])
+		try:
+			parts = self.request.POST['stream'].split('_', 3)
+			recording = Recording.get(parts[1])
+		except:
+			recording = None
+
 		if recording == None:
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="No such stream left for consuming."></red5missioncontrol>');
 			return
 
 		if parts[0] != str(recording.created.strftime('%Y%m%d%H%M%S')) or parts[2] != recording.onetimecode:
 			self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+			self.response.out.write('<?xml version="1.0"?>\r\n');
 			self.response.out.write('<red5missioncontrol error="No such stream left for consuming."></red5missioncontrol>');
 			return
 
 		recording.delete()
 		self.response.headers['Content-Type'] = "text/xml; charset=utf-8"
+		self.response.out.write('<?xml version="1.0"?>\r\n');
 		self.response.out.write('<red5missioncontrol><consume /></red5missioncontrol>');
 
 class Test(webapp.RequestHandler):
@@ -104,6 +128,7 @@ class Test(webapp.RequestHandler):
 def main():
 	run_wsgi_app(
 		webapp.WSGIApplication([
+			('/crossdomainpolicy.xml', CrossDomainPolicy),
 			('/record/*', Record),
 			('/record/consume/*', RecordConsume),
 			('/test/*', Test),
